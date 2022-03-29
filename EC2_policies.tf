9230b687892945610c922a901e6b15841a035ed8
poornatest457 <98936171+poornatest457@users.noreply.github.com>
2022-03-29 16:15:39 +0530

    Fix the issue

--- EC2_policies.tf
+++ EC2_policies.tf
@@ -20,6 +20,7 @@ resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
   ingress_cidr_blocks = ["0.0.0.0/16"]
+  monitoring = "true"
   tags = {
     Name = "ExampleAppServerInstance"
   }
